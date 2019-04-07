1cbe7c8f4e493a036172a73746d5d674d9cdb268
Krzysztof Abram <krzysztofabram95@gmail.com>
2019-04-07 20:23:01 +0200

    Change name and UI changes

---
+++ StackOverflowApi Services/App_Start/Bootstrapper.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Http;
+using System.Web.Http.Controllers;
+using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+using Castle.Windsor.Installer;
+using RestServices.Models.Interfaces;
+using RestServices.Services;
+
+namespace RestServices.App_Start
+{
+    public class Bootstrapper
+    {
+        public static void Initialize(WindsorContainer container)
+        {
+         
+            container.Install(FromAssembly.This());
+            container.Register(Classes
+                .FromThisAssembly()
+                .BasedOn<IHttpController>()
+                .ConfigureFor<ApiController>(c => c.Properties(pi => false))
+                .LifestyleTransient());
+
+            container.Register(Castle.MicroKernel.Registration.Component.For<ITagReaderService>()
+                .ImplementedBy(typeof(TagReaderService)));
+
+        }
+        
+    }
+}
---
+++ StackOverflowApi/Controllers/StackOverflowController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+using Mediporta.Controllers.Service;
+
+namespace Mediporta.Controllers
+{
+    public class StackOverflowController : Controller
+    {
+        public ActionResult Index()
+        {
+            var popular = GetDataFromTagService.GetTags(1, 25); 
+
+            return View(popular);
+        }
+    }
+}
